c9b147ce9e07d3bca4a660745e5d9b5be15977c6
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-04-24 15:47:36 +0200

    Creating Discor Bot

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+import responses
+
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+def run_dicord_bot():
+    TOKEN = 'MTEwMDA0NDc3NzA5OTEwNDI4Ng.GNaJNy.ciP1vvqEckbMG_HlGriDgj5tTiMX6KdrYc5rcw'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user: # client.user is bot so we need to avoid infinite loop
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
